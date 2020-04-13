59ca80d4cb2c210de13493f78138415b80d0ee4b
echo7yankee <cosminifrim55@yahoo.com>
2020-04-13 18:29:28 +0300

    UPDATE: Added follow button when visitng a user profile

--- src/ProfileBundle/Components/ProfileImageEditButton/ProfileImageEditButton.jsx
+++ src/ProfileBundle/Components/ProfileImageEditButton/ProfileImageEditButton.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 //style
 import style from './profileImageEditButton.module.css';
 
-export const ProfileImageEditButton = ({ openModal }) => {
+export const ProfileImageEditButton = ({ openModal, isOwner }) => {
   return (
-    <button className={style.profileImageEditButton} onClick={openModal}>
-      Edit Profile
+    <button
+      className={style.profileImageEditButton}
+      onClick={isOwner === null ? openModal : null}>
+      {isOwner === null ? 'Edit Profile' : 'Follow'}
     </button>
   )
 }
