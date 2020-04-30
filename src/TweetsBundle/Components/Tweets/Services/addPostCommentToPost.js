e35e35023772eea5fdb66ea27c6146a8ea563226
echo7yankee <cosminifrim55@yahoo.com>
2020-04-30 13:36:29 +0300

    UPDATE: Changed order of how to post replys add

--- src/TweetsBundle/Components/Tweets/Services/addPostCommentToPost.js
+++ src/TweetsBundle/Components/Tweets/Services/addPostCommentToPost.js
@@ -2,13 +2,13 @@
 export const addPostCommentToPost = (postObj, comment, user) => {
   return {
     ...postObj,
-    postComments: [...postObj.postComments, {
+    postComments: [{
       ...comment,
       username: `${user.fName} ${user.lName}`,
       userId: user.id,
       postId: postObj.id,
       profileImg: user.profileImg,
       createdAt: new Date(),
-    }]
+    }, ...postObj.postComments]
   }
 }
