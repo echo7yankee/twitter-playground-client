89fcbcb152a9f26f21dfc60f88dcebd24024524f
echo7yankee <cosminifrim55@yahoo.com>
2020-05-05 20:23:03 +0300

    UPDATE: Added userInSearchDummy & some style changes in userInSearchDummy

--- src/MessagesBundle/Components/MessagesView/MessagesView.jsx
+++ src/MessagesBundle/Components/MessagesView/MessagesView.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 //style
 // import style from './messages.module.css';
 //Components
 import { MessagesSearch } from '../MessagesSearch/MessagesSearch';
 import { UsersInMessages } from '../UsersInMessages/UsersInMessages';
+import { UsersInSearchDummy } from '../../../GlobalComponents/Dummies/UsersInSearchDummy/UsersInSearchDummy';
 
 export const MessagesView = ({ user }) => {
   return (
-    user.id
-      ? <div>
-        <MessagesSearch
-          user={user}
-        />
-        {!user.social.usersToMessage.length
-          && <div className="divider" style={{ height: '1px' }} />}
-        <UsersInMessages users={user.social.usersToMessage} />
-      </div>
-      : null
+    <div>
+      <MessagesSearch
+        user={user}
+      />
+      {user.id && !user.social.usersToMessage.length
+        && <div className="divider" style={{ height: '1px' }} />}
+      {user.id
+        ? <UsersInMessages users={user.social.usersToMessage} />
+        : <>
+          <UsersInSearchDummy />
+          <UsersInSearchDummy />
+          <UsersInSearchDummy />
+        </>
+      }
+    </div>
+
   )
 }
