0241342772854b5e0f21ca95e46d04e2853cd463
echo7yankee <cosminifrim55@yahoo.com>
2020-05-06 13:19:28 +0300

    UPDATE: Added a search filter when searching for the users that are added in usersToMessage property from user.

--- src/MessagesBundle/Components/MessagesSearch/MessagesSearch.jsx
+++ src/MessagesBundle/Components/MessagesSearch/MessagesSearch.jsx
@@ -4,14 +4,14 @@ import style from './messagesSearch.module.css';
 //utils/assets/constants/services
 //Components
 import { GlobalSearch } from '../../../GlobalComponents/GlobalSearch/GlobalSearch';
-export const MessagesSearch = ({ onChange, search }) => {
+export const MessagesSearch = ({ onChange, value }) => {
   return (
     <div className={style.messagesSearch}>
       <GlobalSearch
         type='text'
         placeholder='Search for people and groups'
         onChange={onChange}
-        value={search}
+        value={value}
         name='search'
       />
     </div>
--- src/MessagesBundle/Components/MessagesView/MessagesView.jsx
+++ src/MessagesBundle/Components/MessagesView/MessagesView.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 //style
 // import style from './messages.module.css';
+//utils
+import { filterUsersBySearch } from '../../Services/filterUsersBySearch';
 //Components
 import { MessagesSearch } from '../MessagesSearch/MessagesSearch';
 import { UsersInMessages } from '../UsersInMessages/UsersInMessages';
 import { UsersInSearchDummy } from '../../../GlobalComponents/Dummies/UsersInSearchDummy/UsersInSearchDummy';
 
 export const MessagesView = ({ user }) => {
+  const [search, setSearch] = useState('');
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
+
   return (
     <div>
       <MessagesSearch
         user={user}
+        onChange={handleChange}
+        value={search}
       />
       {user.id && !user.social.usersToMessage.length
         && <div className="divider" style={{ height: '1px' }} />}
       {user.id
-        ? <UsersInMessages users={user.social.usersToMessage} />
+        ? <UsersInMessages users={filterUsersBySearch(user.social.usersToMessage, search)} />
         : <>
           <UsersInSearchDummy />
           <UsersInSearchDummy />
