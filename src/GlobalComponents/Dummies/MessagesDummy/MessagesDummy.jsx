edaee2658e8b8d71b0cd7f882dd3759973281bb0
echo7yankee <cosminifrim55@yahoo.com>
2020-06-21 16:50:56 +0300

    UPDATE: Added dummy messages when loading messages with a user

---
+++ src/GlobalComponents/Dummies/MessagesDummy/MessagesDummy.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+//style
+import style from './messagesDummy.module.css';
+
+export const MessagesDummy = () => {
+  return (
+    <div className={style.messagesDummyContainer}>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 23 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 32 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 15 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 45 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 29 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 45 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 10 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 12 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 12 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 18 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 32 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyLeft}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 39 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 10 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 36 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+      <div className={style.messagesDummyRight}>
+        <div className={style.messagesDummyProfile}></div>
+        <div
+          style={{ width: 21 + 'rem' }}
+          className={style.messagesDummyText}></div>
+      </div>
+    </div>
+  )
+}
